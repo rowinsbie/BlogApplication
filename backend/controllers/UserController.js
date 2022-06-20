4f8d552b114191096ff216586ac0994c80aa3654
Seiki Rowins Bie <61301846+seikirowinsbie@users.noreply.github.com>
2022-06-20 21:32:51 +0800

    setup JWT Authentication

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,28 +2,52 @@ const asyncHandler = require('express-async-handler');
 const User = require('../model/UserModel');
 let bcrypt = require('bcrypt');
 
-const createUser = asyncHandler(async(req,res) => {
-    
+const createUser = asyncHandler(async(req, res) => {
+
+    // check if user already exists
+    // const { username } = req.body;
+    // const isUserExists = await User.find({ username })
+    // if (isUserExists) {
+    //     res.status(400);
+    //     throw new Error("User already exists");
+    // }
+    // hash the password
+
+    const salt = await bcrypt.genSalt(10);
+    const HashPassword = await bcrypt.hash(req.body.password, salt);
     const newUser = await User.create({
-        first_name:req.body.first_name,
-        last_name:req.body.last_name,
-        username:req.body.username,
-        password:bcrypt.hashSync(req.body.password,11)
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        username: req.body.username,
+        password: HashPassword
     });
 
-    res.json({
-        newUser:newUser
-    })
+    if (newUser) {
+        res.status(200).json({
+            newUser: newUser
+        });
+    } else {
+        res.status(400);
+        throw new Error("Failed to create new user");
+    }
 
 });
 
-const getUsers = asyncHandler(async(req,res) => {
+const getUserData = asyncHandler(async(req, res) => {
     res.status(200).json({
-        users:await User.find()
+        users: await User.find()
     });
 });
+
+
+const login = asyncHandler(async(req, res) => {
+    res.status(200).json({
+        mess: "login"
+    });
+})
 
 module.exports = {
     createUser,
-    getUsers
+    getUserData,
+    login
 }
--- backend/model/BlogModel.js
+++ backend/model/BlogModel.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 const BlogSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
     title: {
         type: String,
         required: [true, 'Please add title']
