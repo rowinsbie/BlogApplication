a6b70d3033c6aea17c266e178f00d2a2d5cc9c37
Seiki Rowins Bie <rowinsbie@gmail.com>
2022-06-28 14:00:00 +0800

    Init UI

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,7 @@
+const Dashboard = () => {
+    <>
+        <h1>Dashboard</h1>
+    </>
+}
+
+export default Dashboard;
---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,8 @@
+const Login = () => {
+    <>
+        <h1>This is login</h1>
+    </>
+}
+
+export default Login;
+
