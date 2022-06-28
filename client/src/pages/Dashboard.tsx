0a72ba1c4696772d99c30f652c4689dbcfa7a663
Seiki Rowins Bie <rowinsbie@gmail.com>
2022-06-28 17:43:05 +0800

    test tailwind if its working

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,9 +1,20 @@
+import Logo from '../images/logo.jpg';
 const Dashboard = () => {
   return (
     <>
-<h1 className="underline text-red-500">
-    Hello fuck!
-  </h1>    </>
+    <div className="container-fluid mx-auto px-4 border p-3 flex">
+    <img src={Logo} alt="logo" width="5%" />
+      <ul className='flex pt-5'>
+        <li className='pr-6'>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+      </ul>
+    
+    </div>
+   </>
   );
 };
 
